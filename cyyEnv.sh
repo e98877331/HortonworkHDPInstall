398daefbc54e4e2eb8b9ca437d9f780c1d5d3862
a100 <a100@LNNN02.(none)>
2014-11-14 14:17:23 +0800

    finally finished version 1

--- cyyEnv.sh
+++ cyyEnv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CYYROOT=.
+CYYROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #cyy add for hadoop 
 source $CYYROOT/hdp_manual_install_rpm_helper_files-2.1.5.695-1/scripts/usersAndGroups.sh
 source $CYYROOT/hdp_manual_install_rpm_helper_files-2.1.5.695-1/scripts/directories.sh
---
+++ reloadConfig.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+sudo rm -rf /etc/hadoop/conf
+
+sudo cp -r ./hdp_manual_install_rpm_helper_files-2.1.5.695-1/configuration_files/core_hadoop /etc/hadoop/conf
+
+sudo chown hdfs:hadoop /etc/hadoop/conf
+sudo chmod 775 /etc/hadoop/conf
+
+
+
+sudo rm -r $ZOOKEEPER_CONF_DIR ;
+sudo mkdir -p $ZOOKEEPER_CONF_DIR ;
+
+sudo cp -r hdp_manual_install_rpm_helper_files-2.1.5.695-1/configuration_files/zookeeper/* $ZOOKEEPER_CONF_DIR 
+
+sudo chmod a+x $ZOOKEEPER_CONF_DIR/;
+sudo chown -R $ZOOKEEPER_USER:$HADOOP_GROUP $ZOOKEEPER_CONF_DIR/../  ;
+sudo chmod -R 755 $ZOOKEEPER_CONF_DIR/../
+
+
+
+
+sudo rm -r $HBASE_CONF_DIR ;
+sudo mkdir -p $HBASE_CONF_DIR ;
+
+
+sudo cp -r hdp_manual_install_rpm_helper_files-2.1.5.695-1/configuration_files/hbase/* $HBASE_CONF_DIR/
+
+sudo chmod a+x $HBASE_CONF_DIR/;
+sudo chown -R $HBASE_USER:$HADOOP_GROUP $HBASE_CONF_DIR/../  ;
+sudo chmod -R 755 $HBASE_CONF_DIR/../
+
+
+
+
+sudo rm -r $PIG_CONF_DIR
+sudo mkdir -p $PIG_CONF_DIR
+
+sudo cp -r hdp_manual_install_rpm_helper_files-2.1.5.695-1/configuration_files/pig/* $PIG_CONF_DIR
+
+sudo chmod -R 755 $PIG_CONF_DIR
+
+
+
